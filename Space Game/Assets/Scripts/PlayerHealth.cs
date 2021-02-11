7a3cc7ce0654e70552fe6613e71b01733738894f
FarrylC <changfa@sheridancollege.ca>
2021-02-11 10:35:46 -0500

    Resolve merge conflicts

---
+++ Space Game/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth;
+    public int health;
+
+    public Rigidbody2D rb;
+
+    public Slider healthBar;
+
+    private float _invincibleTime = 1.0f;
+    private bool _isInvincible = false;
+    private bool _damaged;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        setHealth(maxHealth);
+
+        //Set start value of health bar
+        healthBar.maxValue = maxHealth;
+        healthBar.value = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        setHealth(health);
+    }
+
+    public void setHealth(int _health)
+    {
+        health = _health;
+
+        //Health cannot excede max health
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+
+        //When health reaches 0, player Dies
+        else if (health <= 0)
+        {
+            health = 0;
+        }
+
+        //Update health bar value
+        healthBar.value = _health;
+        print("Value:" + healthBar.value);
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // If the collided object is an asteroid, take damage
+        if (collision.gameObject.CompareTag("Asteroid"))
+        {
+            if (!_isInvincible)
+            {
+                _damaged = true;
+                
+                setHealth(health - 1);
+
+                SetInvincible();
+            }
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Asteroid"))
+        {
+            if (!_isInvincible)
+            {
+                _damaged = true;
+
+                setHealth(health - 1);
+
+                SetInvincible();
+            }
+        }
+    }
+
+    //Stop player from taking damage for a given time.
+    public void SetInvincible()
+    {
+        _isInvincible = true;
+
+        //Stops the SetDamageable() function.
+        CancelInvoke("SetDamageable");
+
+        //Restarts the SetDamageable() funtion.
+        Invoke("SetDamageable", _invincibleTime);
+    }
+
+    void SetDamageable()
+    {
+        _isInvincible = false;
+    }
+
+}
---
+++ Space Game/Assets/Scripts/UIController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject blackOutSquare;
+    public GameObject crack;
+
+    public PlayerHealth playerHealth;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //start coroutine if player has no health
+        if (playerHealth.health <= 0) 
+        {
+            StartCoroutine(FadeBlackOutSquare());
+        }
+
+        //stop coroutine if player has health
+        if (playerHealth.health != 0)
+        {
+            StopCoroutine(FadeBlackOutSquare());
+        }
+    }
+    
+
+    //Fade out the screen and restart the scene
+    public IEnumerator FadeBlackOutSquare(bool fadeToBLack = true, int fadeSpeed = 1)
+    {
+        Color objectColor = blackOutSquare.GetComponent<Image>().color;
+        float fadeAmount;
+
+        if (fadeToBLack)
+        {
+            while (blackOutSquare.GetComponent<Image>().color.a < 1)
+            {
+                fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                blackOutSquare.GetComponent<Image>().color = objectColor;
+                yield return null;
+            }
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            fadeToBLack = false;
+        }
+        else
+        {
+            while (blackOutSquare.GetComponent<Image>().color.a > 0)
+            {
+                fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                blackOutSquare.GetComponent<Image>().color = objectColor;
+                yield return null;
+            }
+        }
+    }
+}
