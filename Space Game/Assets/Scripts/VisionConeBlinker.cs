c4186e358cabf31050675bce26e161db29396743
Wilson Chan <chanwils@sheridancollege.ca>
2021-02-19 18:49:23 -0500

    Fix Blinking Animation Issue

---
+++ Space Game/Assets/Scripts/VisionConeBlinker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisionConeBlinker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    SpriteRenderer sr;
+    public Color visionIdleColor;
+    public Color visionAlertColor;
+    public bool test;
+    void Start()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetColor();
+    }
+    void SetColor()
+    {
+    
+
+        if(test)
+        {
+            sr.color = visionAlertColor;
+        }
+        else
+        {
+            sr.color = visionIdleColor;
+        }
+        
+    }
+}
