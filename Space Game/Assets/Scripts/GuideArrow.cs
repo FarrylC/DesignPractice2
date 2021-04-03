e32bfb04041317b926900c8382b6b297a22f46a1
FarrylC <changfa@sheridancollege.ca>
2021-04-03 14:19:50 -0400

    Implement guide arrow

---
+++ Space Game/Assets/Scripts/GuideArrow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuideArrow : MonoBehaviour
+{
+    public Transform goalPos;
+    public Transform playerPos;
+    public AnimationCurve curveX, curveY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 dirToGoal = (goalPos.position - playerPos.position).normalized;
+        transform.up = dirToGoal;
+        transform.localPosition = new Vector2(325 * curveX.Evaluate(dirToGoal.x), 175 * curveY.Evaluate(dirToGoal.y));
+    }
+}
